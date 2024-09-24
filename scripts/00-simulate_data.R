5df03e9af876fe48e78675f8ae1329802b1b40b2
danield424 <danieldu42466@gmail.com>
2024-09-24 01:26:37 -0400

    Sketched plans and simulated TTC Streetcar delay data, including tests of simulated data.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,33 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates streetcar delay time data.
+# Author: Daniel Du
+# Date: September 24, 2024
+# Contact: danielc.du@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(424)
+
+times <- sprintf("%02d:%02d", rep(0:23, each = 60), rep(0:59, 24))
+
+simulated_data <-
+  tibble(
+    StreetcarLine = sample(lines = c("501", "503", "504", "505", "506", "507", 
+                                     "508", "509", "510", "511", "512"),
+      size = 100, replace = TRUE),
+    Time = sample(times,
+      size = 100, replace = TRUE),
+    DelayMins = as.integer(rexp(n = 100, rate=0.1))
+  )
+
+head(simulated_data)
+
 
 
 

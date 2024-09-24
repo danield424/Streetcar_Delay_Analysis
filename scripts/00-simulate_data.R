02ecba7d58a5cb6f9feb2d082d833bd3f699edfe
danield424 <danieldu42466@gmail.com>
2024-09-24 02:02:21 -0400

    Downloaded and saved data from opendatatoronto

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -12,22 +12,48 @@
 library(tidyverse)
 
 #### Simulate data ####
+
 set.seed(424)
 
+# All possible streetcar lines
+lines <- c(501, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512)
+# Generate times between 00:00 and 23:59
 times <- sprintf("%02d:%02d", rep(0:23, each = 60), rep(0:59, 24))
 
+# Simulate data with lines, times, and delay in minutes
 simulated_data <-
   tibble(
-    StreetcarLine = sample(lines = c("501", "503", "504", "505", "506", "507", 
-                                     "508", "509", "510", "511", "512"),
+    StreetcarLine = sample(lines,
       size = 100, replace = TRUE),
     Time = sample(times,
       size = 100, replace = TRUE),
-    DelayMins = as.integer(rexp(n = 100, rate=0.1))
+    DelayMins = as.integer(rexp(n = 100, rate=0.1) + 3)
+    # Delay simulated with exponential distribution, minimum 3 mins
   )
 
-head(simulated_data)
+# Show 20 example entries
+head(simulated_data, 20)
 
 
+## Tests
+# Checks if Streetcar line numbers are valid
+setequal(unique(simulated_data$StreetcarLine), 
+         c(501, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512))
 
+# Checks if all 11 lines are within dataset
+simulated_data$StreetcarLine |>
+  unique() |>
+  length() == 11
+
+# Checks constraints of delay time and delay minutes
+# note in actual data the time format will be in datetime, not strings.
+simulated_data$Time |> min() == "00:00"
+simulated_data$Time |> max() == "23:59" 
+simulated_data$DelayMins |> min() >= 3
+
+# Checks classes:
+is.numeric(simulated_data$StreetcarLine)
+# Check if the 'Time' column is in datetime format
+inherits(simulated_data$Time, c("POSIXct", "POSIXlt"))
+is.numeric(simulated_data$DelayMins)
 
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,28 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Daniel Du
+# Date: 24 September 2024
+# Contact: danielc.du@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Simulated and tested data
+# Any other information needed? N/A
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+package
 
-
+# Find xlsx file
+resources <- list_package_resources("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+datastore_resources <- filter(resources, tolower(format) == 'xlsx')
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+streetcar_delay_data <- filter(datastore_resources, name == "ttc-streetcar-delay-data-2024") %>% get_resource()
 
-         
+write_csv(streetcar_delay_data, "data/raw_data/streetcar_delays2024.csv")
